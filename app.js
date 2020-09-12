372507c99b9be243c65938e8d26f61c8ded565b4
krstnshlmb <shelemba.almaty@gmail.com>
2020-09-12 14:49:40 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -39,9 +39,7 @@ var User = require("./models/user");
 
 var request = require("request");
 
-//-----------------------------------------------------------------------------//
-//-----------------------------------Routes------------------------------------//
-//-----------------------------------------------------------------------------//
+// Routes
 
 var commentRoutes = require("./routes/comments");
 var campgroundRoutes = require("./routes/campgrounds");
@@ -49,35 +47,23 @@ var indexRoutes = require("./routes/index");
 var userRoutes = require("./routes/user");
 var forgotRoutes = require("./routes/forgot");
 
-//-----------------------------------------------------------------------------//
-//-----------------------------Database Connection-----------------------------//
-//-----------------------------------------------------------------------------//
-
-//Live Database             //Local Database                         
+//Database connection
+                    
 var db = process.env.DATABASEURL || "mongodb://localhost/yelpcamp";
-//console.log(process.env.DATABASEURL);
-
-//Starts Connection
 mongoose.connect(db);
 
-//-----------------------------------------------------------------------------//
-//-------------------------------App.use & App.set-----------------------------//
-//-----------------------------------------------------------------------------//
 
-//View Directory Files AAre Automatically Presumed To Be EJS Files
+//Sets view engine as ejs
 app.set("view engine", "ejs");
 
-//Gets Dat From Forms
+//Receives data from forms
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//Links To Public Directory
+//Public directory
 app.use(express.static(__dirname + "/public"));
 
 //Used For Put Request 
 app.use(methodOverride("_method"));
-
-//Test Data - Not Needed Anymore
-//seedDB(); 
 
 //Connect Flash
 app.use(flash());
@@ -94,9 +80,7 @@ app.use(cookieParser());
 
 
 
-//-----------------------------------------------------------------------------//
-//-------------------------------Passport JS Config----------------------------//
-//-----------------------------------------------------------------------------//
+//Passport JS configuration
 
 app.use(require("express-session")({
     secret: "Once Again Rusty Is The Best",
@@ -107,33 +91,22 @@ app.use(require("express-session")({
 app.use(passport.initialize());
 app.use(passport.session());
 
-//Comes From From User.js model
 passport.use(new LocalStrategy(User.authenticate()));
-
-//Creates Session When User Logs In
 passport.serializeUser(User.serializeUser());
-
-//Removes Session When User Logs Out
 passport.deserializeUser(User.deserializeUser());
 
 
-//Custom Middle Ware
+//Custom Middleware
 
-//Gloal Data 
 app.use(function (req, res, next) {
-
-    //Used In Headere.ejs - Shows Current User Which Is Logged In
     res.locals.currentUser = req.user;
-
-
-    //Flash Message Configuratiion - Styled Using Bootstrap
     res.locals.error = req.flash("error");
     res.locals.success = req.flash("success");
     next();
 });
 
 
-//Acts As Placeholder For Routes 
+//"Placeholder" for routes
 app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
@@ -142,17 +115,12 @@ app.use(forgotRoutes);
 
 
 
-//----------------------------------------------------------------------------//
-//-------------------------------404 Route------------------------------------//
-//----------------------------------------------------------------------------//
+//404 - when a non-predefined route is attempted to be accessed
 
 app.get("*", function (req, res) {
     res.render("404");
 });
 
-//----------------------------------------------------------------------------//
-//---------------------------------Server Message-----------------------------//
-//----------------------------------------------------------------------------//
 var port = 3000 ;
 
 app.listen(port, function () {
